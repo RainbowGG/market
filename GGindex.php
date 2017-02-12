f0480bdfbada22c9c464d66f632b6d5517e41417
Oleksandr <oleksandr12@meta.ua>
2017-02-12 14:23:17 +0200

    Добавил гиндекс и ггиндекс

--- GGindex.php
+++ GGindex.php
@@ -2,7 +2,7 @@
 include "header.php";
 ?>
 <div style="text-align: center;">
-<h1>Center</h1>
+<h1>Center1</h1>
 </div>
 <?php
 include "footer.php";?>
---
+++ Gindex.php
@@ -0,0 +1,8 @@
+ <?php
+include "header.php";
+?>
+<div style="text-align: center;">
+<h1>Center1</h1>
+</div>
+<?php
+include "footer.php";?>
